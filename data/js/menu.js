7b6b57bd0060fef9b91b8e5809382f9245e1c366
Sebastian Waldvogel <sebastian@waldvogels.de>
2025-11-02 16:02:50 +0100

    Responsive web UI

--- data/js/menu.js
+++ data/js/menu.js
@@ -1,24 +1,14 @@
-// Title and Menubar
-
-function LoadTitle(containerId) {
-  const container = document.getElementById(containerId);
-  if (!container) return;
-
-  container.innerHTML = `
-    <h1>1-Wire Interface</h1>
-  `;
-}
-
 function LoadMenu(containerId) {
   const container = document.getElementById(containerId);
   if (!container) return;
 
   container.innerHTML = `
     <div class="menubar">
+      <img class="logo" src="img/logo.png">
       <a href="/">Dashboard</a>
       <a href="/config">Configuration</a>
-      <a href="/ota">OTA Update</a>
       <a href="/console">Console</a>
+      <a href="/ota">OTA</a>
       <a href="/logout">Logout</a>
     </div>
   `;
